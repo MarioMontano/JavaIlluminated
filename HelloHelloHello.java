5b2a240d414d04b8ab2e68cef3a82ae1cfa4d0d6
Mario Montano <32518449+MarioMontano@users.noreply.github.com>
2018-06-22 12:57:04 -0500

    Create HelloHelloHello.java

---
+++ HelloHelloHello.java
@@ -0,0 +1,17 @@
+/**
+ * Prints Hello 3 times.
+ *
+ * Mario Montano
+ */
+import java.util.Scanner;
+public class HelloHelloHello
+{
+    public static void main(String [] args)
+    {
+        int i;
+        
+        for (i = 0; i < 3; i++)
+        System.out.println("Hello");
+        System.out.println("The value of i is " + i);
+    }
+}
