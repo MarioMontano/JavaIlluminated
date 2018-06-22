28e917da889fdd339664a4546b5717a5637618c7
Mario Montano <32518449+MarioMontano@users.noreply.github.com>
2018-06-22 12:53:05 -0500

    Create CircumferenceOfCircle.java

---
+++ CircumferenceOfCircle.java
@@ -0,0 +1,15 @@
+/**
+ * Calculates circumference of circle with radius 3.2 inches.
+ *
+ * Mario Montano
+ * 
+ */
+public class CircumferenceofCircle
+{
+   public static void main(String args[])
+   {
+      double radius = 3.2;
+      double circumference= Math.PI * 2 * radius;
+      System.out.println("The circumference of the circle is: " + circumference);
+   }
+}
