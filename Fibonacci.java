eba3ff6679da7be4e30f651b060cb8b475d1e78c
Mario Montano <32518449+MarioMontano@users.noreply.github.com>
2018-06-22 13:07:18 -0500

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,27 @@
+
+/**
+ * 
+ * Creates Fibonacci sequence up to integer.
+ * Mario Montano
+ * 
+ */
+import java.util.Scanner;
+public class Fibonacci
+{  
+    public static void main(String args[])  
+    {    
+        int n1=0,n2=1,n3,i,count;  
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter an integer");
+        count = scan.nextInt( );
+        System.out.print(n1+" "+n2);
+    
+        for(i=2 ; i < count ; i++)    
+        {    
+            n3 = n1 + n2;    
+            System.out.print(" " + n3);    
+            n1 = n2;    
+            n2 = n3;    
+        }     
+    }
+}  
