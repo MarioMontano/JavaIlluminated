c3cbe197f5c4a52afbe470d35bd459ad02e474be
Mario Montano <32518449+MarioMontano@users.noreply.github.com>
2018-06-22 12:35:50 -0500

    Create AverageOfIntegers.java

---
+++ AverageOfIntegers.java
@@ -0,0 +1,21 @@
+/**
+ * average of integers 1, 7, 9, and 34
+ *
+ * Mario Montano
+ * 
+ */
+public class AverageOfIntegers
+{
+    public static void main(String[] args)
+    {
+        int num1, num2, num3, num4;
+        int average, sum;
+        num1 = 1;
+        num2 = 7;
+        num3 = 9;
+        num4 = 34;
+        sum = num1 + num2 + num3 + num4;
+        average = sum/4;
+        System.out.print(" The average of them is : " + average);
+    }
+}
