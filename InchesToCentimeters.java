07be90a6fda0836bfe8229d43a0f9da12ee2b390
Mario Montano <32518449+MarioMontano@users.noreply.github.com>
2018-06-22 12:36:56 -0500

    Create InchesToCentimeters.java

---
+++ InchesToCentimeters.java
@@ -0,0 +1,17 @@
+/**
+ * inches to centimeters
+ *
+ * Mario Montano
+ * 
+ */
+public class inchestocentimeters
+{
+    public static void main(String [] args)
+  {
+      double CM_PER_INCH = 2.54;
+      double inches = 5.2;
+      double centimeters = inches + CM_PER_INCH ;
+      System.out.println(inches + "inches are equivalent to" + centimeters + 
+      " centimeters.");
+    }
+}
