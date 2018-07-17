a03ad7127271cc5f9bda4b8533826af60006ce8c
Mario Montano <32518449+MarioMontano@users.noreply.github.com>
2018-07-17 10:36:30 -0500

    Create SumIntegersOneToFive.java

---
+++ SumIntegersOneToFive.java
@@ -0,0 +1,18 @@
+/**
+ * This loop calculates the sum of the integers from 1 to 5 using a for loop.
+ *
+ * Mario Montano
+ * 
+ */
+public class SumIntegersOneToFive
+{
+    public static void main( String [ ] args )
+    {
+        int sum = 0;
+        for( int i = 0; i <= 5; i++ )
+        {
+            sum += i;
+        }
+        System.out.println( sum );
+    }
+}
