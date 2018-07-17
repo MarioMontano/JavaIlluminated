9a7b4f0408da922d04456ae5c38de4628c1a555c
Mario Montano <32518449+MarioMontano@users.noreply.github.com>
2018-07-17 08:10:10 -0500

    Create PCGameClient.java

---
+++ PCGameClient.java
@@ -0,0 +1,15 @@
+/**
+ * PCGame Client
+ *
+ * Mario Montano
+ * 
+ */
+public class PCGameClient
+{
+    public static void main( String [ ] args )
+    
+    {     
+        PCGame p2 = new PCGame( "Fortnite" );
+        System.out.println( "New PC game: " + p2 );
+    }        
+}
