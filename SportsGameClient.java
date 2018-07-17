c8e096c0dfb8eca60e3e78224435cdb82ae6a064
Mario Montano <32518449+MarioMontano@users.noreply.github.com>
2018-07-17 08:11:04 -0500

    Create SportsGameClient.java

---
+++ SportsGameClient.java
@@ -0,0 +1,15 @@
+/**
+ * Client to test SportsGame class.
+ *
+ * Mario Montano
+ * 
+ */
+public class SportsGameClient
+{
+    public static void main( String [ ] args )
+    
+    {     
+        SportsGame s2 = new SportsGame( "Football" );
+        System.out.println( "New PC game: " + s2 );
+    }     
+}
